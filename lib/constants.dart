f382ec40d60ba7d5c08a88db00455666c2deecaf
imamuna <USER@IMAMUNA>
2022-08-05 01:48:37 +0200

    Initial commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,3 +6,21 @@ const kTextColor = Color(0XFF3C4046);
 const kBackgroundColor = Color(0XFFF9F8FD);
 
 const double kDefaultPadding = 20.0;
+
+class Palette {
+  static const MaterialColor kToDark = MaterialColor(
+    0xff0c9869, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
+    <int, Color>{
+      50: Color(0xff0b895f), //10%
+      100: Color(0xff0a7a54), //20%
+      200: Color(0xff086a4a), //30%
+      300: Color(0xff075b3f), //40%
+      400: Color(0xff064c35), //50%
+      500: Color(0xff053d2a), //60%
+      600: Color(0xff042e1f), //70%
+      700: Color(0xff021e15), //80%
+      800: Color(0xff010f0a), //90%
+      900: Color(0xff000000), //100%
+    },
+  );
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset('icons/menu.svg'),
+        onPressed: () {},
+      ),
+    );
   }
 }
